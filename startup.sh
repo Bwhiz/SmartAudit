3dd4538040cf212eb048d1e3f45775b5314c2393
Benedict O. Ejelonu <59194091+Bwhiz@users.noreply.github.com>
2024-11-05 15:41:43 +0000

    created startup bash script

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set Streamlit server settings
+export STREAMLIT_PORT=${PORT:-8501}
+export STREAMLIT_SERVER_HEADLESS=true
+
+# Run Streamlit
+streamlit run app.py --server.port $STREAMLIT_PORT --server.address 0.0.0.0
